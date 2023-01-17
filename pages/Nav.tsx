d452d4d37b9526d2acfab9ef2531ba1df0247702
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-17 01:03:23 +0000

    adds nav components

--- pages/Nav.tsx
+++ pages/Nav.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className='bg-white shadow-lg w-full fixed z-20 '>
+        <div className='w-full'>
+            <div className='flex items-center h-20 w-full'>
+                <div className='flex items-center mx-10 justify-between w-full'>
+                    <div>H1</div>
+                    <div>h2</div>
+                    <div>h3</div>
+                </div>
+            </div>
+        </div>
+    </nav>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Nav from './Nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
+        <Nav/>
       </main>
     </>
   )
