d9f5bf790b9d6f05936b1564f73a181114a75d6b
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-18 22:19:58 +0000

    adds password label

--- pages/Signin.tsx
+++ pages/Signin.tsx
@@ -13,7 +13,16 @@ function Signin() {
                     <form action="">
                         <div className='relative mt-6'>
                             <input type="email" name='email' id='email' placeholder='Email Address' className='peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500m focus:outline-none' autoComplete='NA' />
+                            <label htmlFor="email" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Email Address</label>
                         </div>
+                        <div className='relative mt-6'>
+                            <input type="password" name='password' id='password' placeholder='Password' className='peer border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500m focus:outline-none' />
+                            <label htmlFor="password" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Password</label>
+                        </div>
+                        <div className="my-6">
+                            <button type="submit" className="w-full rounded-md bg-black px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign in</button>
+                        </div>
+                        <p className="text-center text-sm text-gray-500">Don&#x27;t have an account yet? <a href="#!" className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none">Sign up</a>.</p>
                     </form>
                 </div>
             </div>
