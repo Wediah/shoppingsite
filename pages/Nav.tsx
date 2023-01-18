e742a248f59b57649c86ba507b0a034a59d862d2
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-18 09:42:27 +0000

    adds styling to search button

--- pages/Nav.tsx
+++ pages/Nav.tsx
@@ -9,10 +9,10 @@ function Nav() {
         <div className='w-full'>
             <div className='flex items-center h-20 w-full'>
                 <div className='flex items-center mx-10 justify-between w-full'>
-                    <div>
-                      <div>
-                        <div><TbSearch className='text-2xl hover:scale-75 duration-100 cursor-pointer'/></div>
-                        <input type="text" placeholder='Search' className='w-full h-10 outline-none border-none text-md font-semibold rounded-full bg-gray-300 p-2xbn focus:outline-none placeholder:text-slate-400 placeholder:italic placeholder:focus:opacity-0 placeholder:transition-all placeholder:ease-in-out placeholder:duration-75' />
+                    <div className='flex h-10 bg-gray-300 rounded-xl overflow-hidden text-center'>
+                      <div className='flex items-center relative '>
+                        <div><TbSearch className='text-2xl hover:scale-75 duration-100 cursor-pointer align-middle m-2'/></div>
+                        <input type="text" placeholder='Search' className='w-full h-10 outline-none border-none text-md font-semibold bg-gray-300 focus:outline-none placeholder:text-slate-600 placeholder:italic placeholder:focus:opacity-0 placeholder:transition-all placeholder:ease-in-out placeholder:duration-75' />
                       </div>
                     </div>
                     <div>
