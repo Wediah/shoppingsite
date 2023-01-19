6d743d5da082646d346be3bd4cdfedd1b06c9624
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-19 11:01:54 +0000

    adds alternative to sign up

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Nav from './Nav'
 import { useRouter } from 'next/router'
+import {BsApple} from 'react-icons/bs'
+import {FcGoogle} from 'react-icons/fc'
 
 function Signup() {
     const router = useRouter()
@@ -53,6 +55,11 @@ function Signup() {
                                 <button type="submit" className="w-full rounded-md bg-black px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign up</button>
                             </div>
                             <p className="text-center text-sm text-gray-500">Already having an account? <a href="#!" className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none" onClick={() => router.push('/Signin')}>Sign in</a>.</p>
+                            <p className='text-center text-sm text-gray-500'>or</p>
+                            <div className='flex gap-5 justify-center mt-2'>
+                                <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><FcGoogle className='text-lg align-middle mr-1 '/>Sign up with Google</button>
+                                <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><BsApple className='text-lg align-middle mr-1'/>Continue with Apple</button>
+                            </div>
                         </form>
                     </div>
                 </div>
