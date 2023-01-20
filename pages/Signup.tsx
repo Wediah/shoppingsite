63d6dd84aa0ab5dbd82c26d559183e5e361ce23f
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-20 23:11:52 +0000

    edits password reentery

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -30,7 +30,7 @@ function Signup() {
                             </div>
                             <div className='relative mt-6'>
                                 <input type="password" name='password' id='password' placeholder='Password' className='peer w-full rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none" autocomplete="off"'/>
-                                <label htmlFor="password" className='pointer-events-none absolute top-0 left-0 ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800'>Enter Password Again</label>
+                                <label htmlFor="password" className='pointer-events-none absolute top-0 left-0 ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800'>Confirm Password</label>
                             </div>
                             
                             <div className="my-6">
