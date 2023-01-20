f585cdd9b67ec912aadc53fd6b36541ca1bb41e9
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-20 23:07:25 +0000

    adds firebase config

---
+++ pages/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkEeR-Y6m-t36s1yHObDXABgn8RCF80kc",
+  authDomain: "fir-user-auth-65e83.firebaseapp.com",
+  projectId: "fir-user-auth-65e83",
+  storageBucket: "fir-user-auth-65e83.appspot.com",
+  messagingSenderId: "1053658559137",
+  appId: "1:1053658559137:web:e641329134237eae0436e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export {auth}
