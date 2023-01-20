4b1adb13e0a2ebee76be27ce2e7851c06f7b60ae
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-20 23:27:20 +0000

    removes validation

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -10,41 +10,7 @@ import {createUserWithEmailAndPassword, sendEmailVerification} from 'firebase/au
 function Signup() {
     const router = useRouter()
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [error, setError] = useState('')
-  
-    const validatePassword = () => {
-      let isValid = true
-      if (password !== '' && confirmPassword !== ''){
-        if (password !== confirmPassword) {
-          isValid = false
-          setError('Passwords does not match')
-        }
-      }
-      return isValid
-    }
-  
-    const register = e => {
-      e.preventDefault()
-      setError('')
-      if(validatePassword()) {
-        // Create a new user with email and password using firebase
-          createUserWithEmailAndPassword(auth, email, password)
-          .then(() => {
-            sendEmailVerification(auth.currentUser)   
-            .then(() => {
-              setTimeActive(true)
-              navigate('/verify-email')
-            }).catch((err) => alert(err.message))
-          })
-          .catch(err => setError(err.message))
-      }
-      setEmail('')
-      setPassword('')
-      setConfirmPassword('')
-    }
+    
 
 
   return (
