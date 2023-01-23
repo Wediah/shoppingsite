3c1771269463f980c1342321a338354d21e406ee
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-23 22:45:20 +0000

    adds landing to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Nav from './Nav'
+import Landing from './Landing'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,6 +22,7 @@ export default function Home() {
       </Head>
       <main>
         <Nav/>
+        <Landing/>
       </main>
     </>
   )
