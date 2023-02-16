19e491674f17c202102227e322361707f069d47f
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:25:42 +0000

    removes text and adds bg

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -5,9 +5,8 @@ function Landing() {
     <div className='min-h-screen pt-40'>
 
       <div className=''>
-        <div className='bg-no-repeat  bg-cover bg-center  bg-black  " style={{ backgroundImage: 'url()'}}'>
-          Get 20% off any purchase above $200 <br />
-          <button type='button' title='s'>shop now!</button>
+        <div className=" bg-no-repeat  bg-cover bg-center  bg-black  " style={{ backgroundImage: 'url(pjbg1.svg)'}}>
+          
         </div>
         <div></div>
         <div></div>
