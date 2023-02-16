1f496f73592e2ade732b3cec40b30300e56b5bf0
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:15:50 +0000

    adss an outline

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className='min-h-screen pt-40'>
+    <div className='min-h-screen pt-40 px-10'>
 
       <div className=''>
-        <div className='w-screen text-black text-6xl text-center'>
-          Get 20% off any purchase above $200
+        <div className='w-screen rounded-xl text-black text-6xl text-center outline outline-offset-2'>
+          Get 20% off any purchase above $200 <br />
           <button type='button' title='s'>shop now!</button>
         </div>
         <div></div>
