f4e7c199b4f6be92b4a88f6bacd5d65a866fa940
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:35:15 +0000

    adds a button

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -6,7 +6,10 @@ function Landing() {
 
       <div className=''>
         <div className=" bg-no-repeat  bg-cover bg-center  bg-black  " style={{ backgroundImage: 'url(pjbg1.svg)'}}>
-          
+          <div className='text-6xl font-bold p-28 text-white'>
+            Get 20% OFF! <br /> When You Purchase above $100 <br />
+            <button className='text-black font-bold text-xl outline-white outline hover:outline-none outline-offset-4 rounded-full p-3 bg-white ' type='button' title='s'>Shop Now!</button>
+          </div>
         </div>
         <div></div>
         <div></div>
