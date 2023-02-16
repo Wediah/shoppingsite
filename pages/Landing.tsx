f12496d196b9a4722463957e9c44c94da9492ed5
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:06:43 +0000

    adds text to first div

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen pt-40'>
 
       <div>
-        <div></div>
+        <div className='w-screen text-black text-6xl'>
+          Get 20% off any purchase above $200
+          <button type='button' title='s'>shop now!</button>
+        </div>
         <div></div>
         <div></div>
       </div>
