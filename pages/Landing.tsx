74257629f8fd3dfa420caeaec7624f521d4f80b0
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:20:57 +0000

    adds an svg to public

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className='min-h-screen pt-40 px-10'>
+    <div className='min-h-screen pt-40'>
 
       <div className=''>
         <div className='w-screen rounded-xl text-black text-6xl text-center outline outline-offset-2'>
