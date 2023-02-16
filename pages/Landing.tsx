0528e3c1d88f7db6180bcef8224c984919f619b6
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:08:14 +0000

    centers text

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -4,8 +4,8 @@ function Landing() {
   return (
     <div className='min-h-screen pt-40'>
 
-      <div>
-        <div className='w-screen text-black text-6xl'>
+      <div className=''>
+        <div className='w-screen text-black text-6xl text-center'>
           Get 20% off any purchase above $200
           <button type='button' title='s'>shop now!</button>
         </div>
