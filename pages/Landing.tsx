8bd27a2960635d8751a6392145a6670bb3189743
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 22:49:51 +0000

    adds title to button

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className='min-h-screen pt-40'>
+    <div className='min-h-screen pt-36 px-10'>
 
       <div className=''>
         <div className=" bg-no-repeat  bg-cover bg-center  bg-black  " style={{ backgroundImage: 'url(pjbg1.svg)'}}>
           <div className='text-6xl font-bold p-28 text-white'>
             Get 20% OFF! <br /> When You Purchase above $100 <br />
-            <button className='text-black font-bold text-xl outline-white outline hover:outline-none outline-offset-4 rounded-full p-3 bg-white ' type='button' title='s'>Shop Now!</button>
+            <button className='text-black font-bold text-xl outline-white outline hover:outline-none outline-offset-4 rounded-full p-3 bg-white ' type='button' title='shop now'>Shop Now!</button>
           </div>
         </div>
         <div></div>
