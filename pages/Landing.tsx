03527c7d9d431c06652422ec153c73723d0f1b47
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-16 23:08:15 +0000

    adds a carousel

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -6,7 +6,59 @@ function Landing() {
   return (
     <div className='min-h-screen pt-36 px-10'>
 
-      <div className=''>
+      <Carousel 
+      additionalTransfrom={0}
+      arrows={false}
+      autoPlay
+      autoPlaySpeed={10000}
+      centerMode={false}
+      className=""
+      containerClass="container-with-dots"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024
+          },
+          items: 1,
+          partialVisibilityGutter: 40
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0
+          },
+          items: 1,
+          partialVisibilityGutter: 30
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464
+          },
+          items: 1,
+          partialVisibilityGutter: 30
+        }
+      }}
+      rewind={false}
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={false}
+      sliderClass=""
+      slidesToSlide={2}
+      swipeable>
         <div className=" bg-no-repeat  bg-cover bg-center  bg-black  " style={{ backgroundImage: 'url(pjbg1.svg)'}}>
           <div className='text-6xl font-bold p-28 text-white'>
             Get 20% OFF! <br /> When You Purchase above $100 <br />
@@ -17,7 +69,7 @@ function Landing() {
 
         </div>
         <div></div>
-      </div>
+      </Carousel>
 
     </div>
   )
