7c21a4a13e584353c796efc22ee1a56aada73027
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-19 23:52:48 +0000

    adds legacy prop

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 
 function Landing() {
   return (
